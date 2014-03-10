ba9052c315b2df6ee327d4a23738de700fab5623
Dees Troy <dees_troy@teamw.in>
2014-03-10 15:42:45 +0000

    Add TARGET_CPU_VARIANT for newer trees

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a5
+TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
