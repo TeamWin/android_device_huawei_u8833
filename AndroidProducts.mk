aae2f1abcc2c612e19546412d1a4be146dc68d08
Kra1o5 <kra1o5x@gmail.com>
2014-03-11 19:21:27 +0100

    More updates

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/u8833.mk
+    $(LOCAL_DIR)/u8833.mk \
+    $(LOCAL_DIR)/omni_u8833.mk
--- omni_u8833.mk
+++ omni_u8833.mk
@@ -1,15 +1,15 @@
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Release name
+PRODUCT_RELEASE_NAME := u8833
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration.
 $(call inherit-product, device/huawei/u8833/u8833.mk)
 
 # Device identifier.
 PRODUCT_DEVICE := u8833
-PRODUCT_NAME := cm_u8833
+PRODUCT_NAME := omni_u8833
 PRODUCT_BRAND := huawei
 PRODUCT_MODEL := Ascend Y300
 PRODUCT_MANUFACTURER := huawei
